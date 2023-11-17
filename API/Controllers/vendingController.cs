1918926bf06112c1f77e4f3a374cb94f77e97e85
daknox <daknox@crimson.ua.edu>
2023-11-17 09:50:36 -0600

    CRUD For the machineController

--- API/Controllers/vendingController.cs
+++ API/Controllers/vendingController.cs
@@ -1,5 +1,11 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;  
+using System.Collections.Generic;  
+using System;  
+using Microsoft.AspNetCore.Http;
+using mis321_ttv_web_app;
+using mis321_ttv_web_app.API.Models;
 
 namespace MyApp.Namespace
 {
@@ -7,31 +13,147 @@ namespace MyApp.Namespace
     [ApiController]
     public class vendingController : ControllerBase
     {
+
+         private readonly string cs;
+
+        public vendingController()
+        {
+            cs = new ConnectionString().cs;
+        }
+
         // GET: api/<vending>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Machines> Get()
         {
-            string[] myArray = {"machine 1", "machine2", "machine3"};
-            return new string[] { "value1", "value2" };
+            using (MySqlConnection connection = new MySqlConnection(cs))
+            {
+                connection.Open();
+
+                var machines = new List<Machines>();
+
+                using (MySqlCommand command = new MySqlCommand("SELECT * FROM machine", connection))
+                {
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            machines.Add(new Machines
+                            {
+                                machineId = Convert.ToInt32(reader["machineId"]),
+                                MachineLocation = reader["machineLocation"].ToString(),
+                                MachineRegion = reader["machineRegion"].ToString(),
+                                MachineType = reader["machineType"].ToString()
+                            });
+                        }
+                    }
+                }
+
+                return machines;
+            }
         }
 
         // GET api/<vending>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult Get(int id)
         {
-            return "value";
+            using (MySqlConnection connection = new MySqlConnection(cs))
+            {
+                connection.Open();
+
+                using (MySqlCommand command = new MySqlCommand("SELECT * FROM machine WHERE machineId = @id", connection))
+                {
+                    command.Parameters.AddWithValue("@id", id);
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            Machines machine = new Machines
+                            {
+                                machineId = Convert.ToInt32(reader["machineId"]),
+                                MachineLocation = reader["machineLocation"].ToString(),
+                                MachineRegion = reader["machineRegion"].ToString(),
+                                MachineType = reader["machineType"].ToString()
+                            };
+
+                            return Ok(machine);
+                        }
+                        else
+                        {
+                            return NotFound();
+                        }
+                    }
+                }
+            }
         }
 
         // POST api/<vending>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] Machines machine)
         {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(cs))
+                {
+                    connection.Open();
+
+                    using (MySqlCommand command = new MySqlCommand(
+                        "INSERT INTO machine (machineLocation, machineRegion, machineType) " +
+                        "VALUES (@location, @region, @type)", connection))
+                    {
+                        command.Parameters.AddWithValue("@location", machine.MachineLocation);
+                        command.Parameters.AddWithValue("@region", machine.MachineRegion);
+                        command.Parameters.AddWithValue("@type", machine.MachineType);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+                return Ok("Machine added successfully");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Error: {ex.Message}");
+            }
         }
+
 
         // PUT api/<vending>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(int id, [FromBody] Machines machine)
         {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(cs))
+                {
+                    connection.Open();
+
+                    using (MySqlCommand command = new MySqlCommand(
+                        "UPDATE machine SET machineLocation = @location, machineRegion = @region, machineType = @type " +
+                        "WHERE machineId = @id", connection))
+                    {
+                        command.Parameters.AddWithValue("@id", id);
+                        command.Parameters.AddWithValue("@location", machine.MachineLocation);
+                        command.Parameters.AddWithValue("@region", machine.MachineRegion);
+                        command.Parameters.AddWithValue("@type", machine.MachineType);
+
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            return Ok("Machine updated successfully");
+                        }
+                        else
+                        {
+                            return NotFound("Machine not found");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Error: {ex.Message}");
+            }
         }
 
         // DELETE api/<vending>/5
