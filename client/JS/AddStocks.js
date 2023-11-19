38128ef42ae551f38077407034922dfd2dd71e3f
uyentruong2003 <uyenkaty.truong@gmail.com>
2023-11-19 16:53:46 -0600

    Uyen started fetching api codes

--- client/JS/AddStocks.js
+++ client/JS/AddStocks.js
@@ -1,30 +1,4 @@
-
-// function to set the datalist for the searchable dropdowns:
-function setDataList(dataSource, formComponentId){
-    const dropdown = document.querySelector(`#${formComponentId}-list`);
-    dataSource.forEach((item) => {
-        let option = document.createElement('option');
-        option.value = item;
-        dropdown.appendChild(option);
-    });
-    // An option for Other
-    let option = document.createElement('option');
-    option.value = 'Other';
-    dropdown.appendChild(option);
-}
-
-function displaySelfInputSection(formComponentId) {
-        //handle self input if "Other" is chosen
-        let dropdownInput = document.getElementById(`${formComponentId}`);
-        let otherInputSection = document.getElementById(`other-${formComponentId}`);
-        // let selfInput = document.getElementById(`${formComponentId}-self-input`);
-            if (dropdownInput.value === "Other") {
-                otherInputSection.hidden = false; //unhide the section
-            } else {
-                otherInputSection.hidden = true;
-            }
-}
-
+// handle dropdowns and self input when Other is chosen
 let productNameList = ["Coca-Cola","Sprite","SmartWater","Lay's Potato Chip"];
 setDataList(productNameList,'product-name');
 document.getElementById('product-name').addEventListener('change',()=>{
@@ -45,19 +19,67 @@ document.getElementById('product-category').addEventListener('change',()=>{
 
 
 //handle submit button
-document.getElementById('submit-button').addEventListener('click',(event) =>{
+document.getElementById('submit-button').addEventListener('submit',(event) =>{
     event.preventDefault();
     handleSubmitButton();
 })
 
-// handle the self input for other
+
+//-----------------FUNCTIONS------------------------
+
+// function to set the datalist for the searchable dropdowns:
+function setDataList(dataSource, formComponentId){
+    const dropdown = document.querySelector(`#${formComponentId}-list`);
+    dataSource.forEach((item) => {
+        let option = document.createElement('option');
+        option.value = item;
+        dropdown.appendChild(option);
+    });
+    // add an option for Other
+    let option = document.createElement('option');
+    option.value = 'Other';
+    dropdown.appendChild(option);
+}
+
+// function to hide/ unhide the self input section depending on if the Other option is selected
+function displaySelfInputSection(formComponentId) {
+        let dropdownInput = document.getElementById(`${formComponentId}`); //the searchable dropdown input
+        let otherInputSection = document.getElementById(`other-${formComponentId}`); //the div for self input section
+            if (dropdownInput.value === "Other") {
+                otherInputSection.hidden = false; //unhide the section
+            } else {
+                otherInputSection.hidden = true; //hide the section
+            }
+}
+
+// Function to replace "Other" with the self-input value when "Other" is chosen from dropdown
 function handleSelfInputForOther(formComponentId) {
-    return document.getElementById(`${formComponentId}`).value === "Other" ? 
+    // if statement
+    let value = document.getElementById(`${formComponentId}`).value === "Other" ? 
     document.getElementById(`${formComponentId}-self-input`).value : 
     document.getElementById(`${formComponentId}`).value
-
-    // Add the new product/category/vending machine into the corresponding table in the database using 
+    // Add the new product/category/vending machine into the corresponding table in the database
+    
+    return value;
 }
+
+function addNewProductName(){
+
+}
+
+// POST api to add new data row to the corresponding database table
+async function addToDB(newObj,apiLink) {
+    await fetch(apiLink, { //api link for stock
+        method: "POST",
+        body: JSON.stringify(newObj),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+}
+
+
+
 // function to handle submit button:
 function handleSubmitButton(){
     // create an object and store the user input
@@ -68,9 +90,12 @@ function handleSubmitButton(){
         quantity: document.getElementById('quantity').value,
         description: document.getElementById('description').value
     }
-    // submit the obj to the database
+    // submit the obj to the stock database
     console.log(newStock);
+    // addNewStock(newStock, "#"); --> THEN, re-render in admin dashboard
 }
 
 
 // need a fetch api to get the list of the productNameList and the Vending Machine List and the product category list
+// product name determine the price and description
+// vending machine determines the product category
