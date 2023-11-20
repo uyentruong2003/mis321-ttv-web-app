6517ca9884e3d9c6353a061c11a43305ddf457df
uyentruong2003 <uyenkaty.truong@gmail.com>
2023-11-19 23:12:49 -0600

    Uyen added vending machine validation

--- client/JS/AddNewStocks.js
+++ client/JS/AddNewStocks.js
@@ -1,56 +1,65 @@
 let productList = [
-    {name: 'Coca-Cola', productCategory: 'Beverages', unitPrice: 2.50, description:'Calories 100 kcal'},
-    {name: 'Sprite', productCategory: 'Beverages', unitPrice: 2.50, description:'Calories 102 kcal'},
-    {name: "Lay's Potato Chips", productCategory: 'Snacks', unitPrice: 1.75, description:'Calories 75 kcal'},
-    {name: 'KitKat', productCategory: 'Snacks', unitPrice: 1.75, description:'Calories 80 kcal'}
+    {productName: 'Coca-Cola', productCategory: 'Beverage', unitPrice: 2.50, description:'Calories 100 kcal'},
+    {productName: 'Sprite', productCategory: 'Beverage', unitPrice: 2.50, description:'Calories 102 kcal'},
+    {productName: "Lay's Potato Chips", productCategory: 'Snack', unitPrice: 1.75, description:'Calories 75 kcal'},
+    {productName: 'KitKat', productCategory: 'Snack', unitPrice: 1.75, description:'Calories 80 kcal'}
 ]
 
 let categoryList = [
-    {name: 'Beverages'},
-    {name: 'Snacks'},
-    {name: 'Electronics'},
-    {name: 'Movies'},
-    {name: 'Video Games'}
+    {categoryName: 'Beverage'},
+    {categoryName: 'Snack'},
+    {categoryName: 'Electronics'},
+    {categoryName: 'Movie'},
+    {categoryName: 'Video Game'}
 ]
 
-let vendingMachineList = [
-    {name: 'Vending Machine #1', vmLoc: '123 Dirt Rd, Tuscaloosa, AL 35487', vmRegion: 'Southeast', vmType: 'Beverages'},
-    {name: 'Vending Machine #2', vmLoc: '456 Smith Ave, Milpitas, CA 95035', vmRegion: 'West', vmType: 'Snacks'},
-    {name: 'Vending Machine #3', vmLoc: '78 John St, Chicago, CA 60007', vmRegion: 'Midwest', vmType: 'Snacks'}
+let vmList = [
+    {vmID: '1001', vmLoc: '123 Dirt Rd, Tuscaloosa, AL 35487', vmRegion: 'Southeast', vmType: 'Beverage', vmCap: 10},
+    {vmID: '1002', vmLoc: '456 Smith Ave, Milpitas, CA 95035', vmRegion: 'West', vmType: 'Snack', vmCap: 15},
+    {vmID: '1003', vmLoc: '78 John St, Chicago, CA 60007', vmRegion: 'Midwest', vmType: 'Snack', vmCap: 12}
 ]
 
 // Set Product List
-setDataList(productList,"product-name-list");
+setProductList(productList,"product-name-list");
 // Return the product info based on the product name
 document.getElementById("product-name").addEventListener("change",() => {
     let productName = document.getElementById("product-name").value;
     returnProductInfo(productName, productList);
+    // check if the product category matches the machine type everytime new input is made
+    validateVmType();
 })
 
-// Set Vending Machine List
-setDataList(vendingMachineList, "vending-machine-list");
-// Validate if the machine type is matching with the product category
-validateVendingMachineInput();
-document.getElementById("vending-machine").addEventListener("change",() => {
-
+// Set VM List
+setVmList(vmList, "vending-machine-list");
+document.getElementById('vending-machine').addEventListener("change",() => {
+    // check if the product category matches the machine type everytime new input is made
+    validateVmType();
 })
 
-function validateVendingMachineInput() {
-    let vmType = document.getElementById('vending-machine').value;
-    let productCategory = document.getElementById('vending-machine').value;
-    if (vmType !== productCategory){
-        document.getElementById('vending-machine').value = none;
-        document.getElementById('error-message-vm-input').hidden = false;
+//----------------------FUNCTION----------------------
+
+//function to validate vm:
+function validateVmType() {
+    let vmChosen = document.getElementById('vending-machine');
+    let vmType = "";
+    vmList.forEach((item) => {
+        if (vmChosen.value === item.vmType + "VM #" + item.vmID + " - "+item.vmLoc) {
+            vmType = item.vmType; 
+        }
+    })
+    let productCategory = document.getElementById('product-category');
+    if (vmType !== productCategory.value && vmChosen.value !== "" && productCategory !== "") {
+        document.getElementById('warning-message').hidden = false; //unhide the message
     } else {
-        document.getElementById('vending-machine').value = vmType;
+        document.getElementById('warning-message').hidden = true; //hide the message
     }
 }
 // function to set the datalist for the product name's searchable dropdowns:
-function setDataList(dataList, dataListId){
+function setProductList(productList, dataListId){
     const dropdown = document.querySelector(`#${dataListId}`);
-    dataList.forEach((item) => {
+    productList.forEach((item) => {
         let option = document.createElement('option');
-        option.value = item.name;
+        option.value = item.productName;
         dropdown.appendChild(option);
     });
     // add an option for Other
@@ -59,10 +68,18 @@ function setDataList(dataList, dataListId){
     dropdown.appendChild(option);
 }
 
+function setVmList (vmList, dataListId) {
+    const dropdown = document.querySelector(`#${dataListId}`);
+    vmList.forEach((item) => {
+        let option = document.createElement('option');
+        option.value = item.vmType + "VM #" + item.vmID + " - "+item.vmLoc
+        dropdown.appendChild(option);
+    });
+}
 // function to return the product info based on the product name:
-function returnProductInfo(name, productList) {
+function returnProductInfo(productName, productList) {
     productList.forEach((p) => {
-        if (name === p.name){
+        if (productName === p.productName){
             document.getElementById("product-category").value = p.productCategory;
             document.getElementById("unit-price").value = p.unitPrice;
             document.getElementById("description").value = p.description;
