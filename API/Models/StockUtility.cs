3d1849dfe808fc4d5ec9e37543e87dcba0e39bdb
uyentruong2003 <uyenkaty.truong@gmail.com>
2023-11-23 13:50:36 -0600

    Uyen added utility models

--- API/Models/StockUtility.cs
+++ API/Models/StockUtility.cs
@@ -29,8 +29,39 @@ namespace API
         }
 
         // GET REQUEST
+        public List<Stock> GetAllStocks (){
+            List<Stock> stockList = new List<Stock>();
+            using var con = new MySqlConnection(cs); 
+            con.Open(); 
+                string stm = "SELECT * FROM Stocks"; 
+                using var cmd = new MySqlCommand(stm, con); 
+                using MySqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read()) {
+                    Stock stock = new Stock(){productId = rdr.GetString(0), machineId = rdr.GetString(1),stockQty = rdr.GetInt32(2), lastUpdate = rdr.GetString(3)};
+                    stockList.Add(stock);
+                }
+            return stockList;
+            con.Close();
+        }
+    }
         // PUT REQUEST
+        public void UpdateStock (Stock stock) {
+            using var con = new MySqlConnection (cs);
+            con.Open();
+            using var cmd = new MySqlCommand(con);
+            // Updating the data in the table where id= stock.id:
+            cmd.CommandText = @"UPDATE Stock SET stockQty = @stockQty, lastUpdate = @lastUpdate 
+            WHERE productId = @productId AND machineId = @machineId";
+            cmd.Parameters.AddWithValue("@productId",stock.productId);
+            cmd.Parameters.AddWithValue("@machineId",stock.machineId);
+            cmd.Parameters.AddWithValue("@stockQty",stock.stockQty);
+            cmd.Parameters.AddWithValue("@lastUpdate",stock.lastUpdate);
+            cmd.Prepare();
+            cmd.ExecuteNonQuery();
+            con.Close(); 
+        }
     }
 }
+
 
         
