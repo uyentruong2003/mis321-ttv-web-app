8709cffef18c6922e57ff2375eb470025fe327ab
Ryan <rdlangston1@crimson.ua.edu>
2023-11-28 03:35:46 -0600

    cleaned html admindash

--- API/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8a9185f4c15c2c8ff143783f6352d7460301d08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/JS/AdminInventoryDashBoard.js
+++ client/JS/AdminInventoryDashBoard.js
@@ -2,123 +2,123 @@
 let tempProductList = [];
  
 
-let inventoryData = [
-  {
-    id: 1,
-    name: 'Doritos',
-    categoryid: 1,
-    machineId: 7,
-    qtyInMachine: 10,
-    price: 2.55,
-    region: 'Northwest'
-  },
-  {
-    id: 2,
-    name: 'Coca-Cola',
-    categoryid: 2,
-    machineId: 9,
-    qtyInMachine: 19,
-    price: 1.25,
-    region: 'Northeast'
-  },
-  {
-    id: 3,
-    name: 'Headphones',
-    categoryid: 3,
-    machineId: 4,
-    qtyInMachine: 7,
-    price: 2.25,
-    region: 'Southeast'
-  },
-  {
-    id: 4,
-    name: 'Mountain Dew',
-    categoryid: 1,
-    machineId: 8,
-    qtyInMachine: 15,
-    price: 1.75,
-    region: 'Southwest'
-  },
-  {
-    id: 5,
-    name: 'Laptop',
-    categoryid: 3,
-    machineId: 5,
-    qtyInMachine: 5,
-    price: 899.99,
-    region: 'Midwest'
-  },
-  {
-    id: 6,
-    name: 'Snickers',
-    categoryid: 2,
-    machineId: 6,
-    qtyInMachine: 15,
-    price: 1.50,
-    region: 'Southeast'
-  },
-  {
-    id: 7,
-    name: 'Pepsi',
-    categoryid: 1,
-    machineId: 10,
-    qtyInMachine: 12,
-    price: 1.50,
-    region: 'Northwest'
-  },
-  {
-    id: 8,
-    name: 'Wireless Mouse',
-    categoryid: 3,
-    machineId: 11,
-    qtyInMachine: 8,
-    price: 12.99,
-    region: 'Northeast'
-  },
-  {
-    id: 9,
-    name: 'Chips Ahoy',
-    categoryid: 1,
-    machineId: 12,
-    qtyInMachine: 20,
-    price: 2.00,
-    region: 'Southwest'
-  },
-  {
-    id: 10,
-    name: 'Bluetooth Speaker',
-    categoryid: 3,
-    machineId: 13,
-    qtyInMachine: 10,
-    price: 29.99,
-    region: 'Northeast'
-  },
-  // Add more items as needed
-];
+// let inventoryData = [
+//   {
+//     id: 1,
+//     name: 'Doritos',
+//     categoryid: 1,
+//     machineId: 7,
+//     qtyInMachine: 10,
+//     price: 2.55,
+//     region: 'Northwest'
+//   },
+//   {
+//     id: 2,
+//     name: 'Coca-Cola',
+//     categoryid: 2,
+//     machineId: 9,
+//     qtyInMachine: 19,
+//     price: 1.25,
+//     region: 'Northeast'
+//   },
+//   {
+//     id: 3,
+//     name: 'Headphones',
+//     categoryid: 3,
+//     machineId: 4,
+//     qtyInMachine: 7,
+//     price: 2.25,
+//     region: 'Southeast'
+//   },
+//   {
+//     id: 4,
+//     name: 'Mountain Dew',
+//     categoryid: 1,
+//     machineId: 8,
+//     qtyInMachine: 15,
+//     price: 1.75,
+//     region: 'Southwest'
+//   },
+//   {
+//     id: 5,
+//     name: 'Laptop',
+//     categoryid: 3,
+//     machineId: 5,
+//     qtyInMachine: 5,
+//     price: 899.99,
+//     region: 'Midwest'
+//   },
+//   {
+//     id: 6,
+//     name: 'Snickers',
+//     categoryid: 2,
+//     machineId: 6,
+//     qtyInMachine: 15,
+//     price: 1.50,
+//     region: 'Southeast'
+//   },
+//   {
+//     id: 7,
+//     name: 'Pepsi',
+//     categoryid: 1,
+//     machineId: 10,
+//     qtyInMachine: 12,
+//     price: 1.50,
+//     region: 'Northwest'
+//   },
+//   {
+//     id: 8,
+//     name: 'Wireless Mouse',
+//     categoryid: 3,
+//     machineId: 11,
+//     qtyInMachine: 8,
+//     price: 12.99,
+//     region: 'Northeast'
+//   },
+//   {
+//     id: 9,
+//     name: 'Chips Ahoy',
+//     categoryid: 1,
+//     machineId: 12,
+//     qtyInMachine: 20,
+//     price: 2.00,
+//     region: 'Southwest'
+//   },
+//   {
+//     id: 10,
+//     name: 'Bluetooth Speaker',
+//     categoryid: 3,
+//     machineId: 13,
+//     qtyInMachine: 10,
+//     price: 29.99,
+//     region: 'Northeast'
+//   },
+//   // Add more items as needed
+// ];
  
-let transactions = [
-  {
-    categoryid: 1,
-    productid: 1
-  },
-  {
-    categoryid: 2,
-    productid: 2
-  },
-  {
-    categoryid: 3,
-    productid: 3
-  },
-  {
-    categoryid: 1,
-    productid: 4
-  },
-  {
-    categoryid: 3,
-    productid: 5
-  },
-  // Add more transactions as needed
-];
+// let transactions = [
+//   {
+//     categoryid: 1,
+//     productid: 1
+//   },
+//   {
+//     categoryid: 2,
+//     productid: 2
+//   },
+//   {
+//     categoryid: 3,
+//     productid: 3
+//   },
+//   {
+//     categoryid: 1,
+//     productid: 4
+//   },
+//   {
+//     categoryid: 3,
+//     productid: 5
+//   },
+//   // Add more transactions as needed
+// ];
  
   // END DUMMY DATA
  
@@ -135,21 +135,21 @@ let salesRevenue = '';
  
 //1. API FUNCTIONALITY
  
-// function fetchData(url) {
-//   return fetch(url)
-//       .then(response => response.json())
-//       .catch(error => {
-//           console.error('Error fetching data:', error);
-//       });
-// }
+function fetchData(url) {
+  return fetch(url)
+      .then(response => response.json())
+      .catch(error => {
+          console.error('Error fetching data:', error);
+      });
+}
 
-// fetchData(apiUrl)
-//   .then(apiResponse => {
-//     inventoryData = apiResponse; // Assign the response to inventoryData
-//     populateTable(inventoryData);
-//     console.log(inventoryData);
-//   })
-//   .catch(error => console.error('Error fetching data:', error));
+fetchData(apiUrl)
+  .then(apiResponse => {
+    inventoryData = apiResponse; // Assign the response to inventoryData
+    populateTable(inventoryData);
+    console.log(inventoryData);
+  })
+  .catch(error => console.error('Error fetching data:', error));
  
 // end 1.
  
@@ -162,6 +162,7 @@ function populateTable(data) {
   tableBody.innerHTML = '';
  
   data.forEach(item => {
+    if (item.deleted !== 1) {
     const row = document.createElement('tr');
  
     row.innerHTML = `
@@ -177,18 +178,47 @@ function populateTable(data) {
 <button type="button" class="btn btn-primary" style="background-color: yellowgreen;" onclick="editItem(${item.id}, ${item.machineId})">Edit</button>
 </a>
 <a href="#">
-<button type="button" class="btn btn-primary" style="background-color: red;">Delete</button>
+<button type="button" class="btn btn-primary" style="background-color: red;" onclick="deleteItem(${item.id})">Delete</button>
 </a>
 </td>
     `;
  // ../HTML/EditStocks.html
     tableBody.appendChild(row);
+    }
   });
 }
  
 populateTable(inventoryData);
  
 // END 2.
+
+
+//3. HANDLE DELETE
+
+function deleteItem(productId) {
+  // Send an API request to update the 'deleted' property to 1 for the given product
+  const deleteUrl = `http://localhost:5141/api/AdminDash/${productId}`;
+
+  fetch(deleteUrl, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ deleted: 1 }),
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to delete product');
+      }
+      // If the update is successful, refresh the page
+      location.reload();
+    })
+    .catch(error => {
+      console.error('Error deleting product:', error);
+    });
+}
+
+// end 3
  
  
 console.log(tempProductList);
@@ -199,27 +229,19 @@ let filterChoice = '';
  
 function filterData(categoryid, filterChoice) {
   const filteredItems = inventoryData.filter(item => item.categoryid == filterChoice);
-  tempProductList = []; // Clear the tempProductList
+  tempProductList = [];
   tempProductList.push(...filteredItems);
   populateTable(filteredItems);
- 
-  // Calculate sales revenue based on filtered items
-  let totalSalesRevenue = calculateSalesRevenue(transactions, filteredItems, filterChoice);
-  console.log('Total Sales Revenue for Category', filterChoice, ':', totalSalesRevenue);
- 
+
   // Calculate total available inventory
   let totalAvailableInventory = calculateAvailableInventory(filteredItems);
   console.log('Total Available Inventory for Category', filterChoice, ':', totalAvailableInventory);
- 
-  // Calculate and display sold inventory
-  let totalSoldInventory = calculateSoldInventory(transactions, filterChoice);
-  console.log('Total Sold Inventory for Category', filterChoice, ':', totalSoldInventory);
- 
+
   // Display the calculated values in the HTML
-  document.getElementById('salesRevenueDisplay').textContent = `Sales Revenue: $${totalSalesRevenue.toFixed(2)}`;
-  document.getElementById('availableInventoryDisplay').textContent = `Available Inventory: ${totalAvailableInventory}`;
-  document.getElementById('soldInventoryNumber').textContent = `Sold Inventory: ${totalSoldInventory}`;
- 
+  // document.getElementById('salesRevenueDisplay').textContent = `Sales Revenue: $${calculateSalesRevenue(transactions, filteredItems, filterChoice).toFixed(2)}`;
+  document.getElementById('availableInventoryDisplay').textContent = `Available Inventory: ${inventoryAvailable}`;
+  // document.getElementById('soldInventoryNumber').textContent = `Sold Inventory: ${calculateSoldInventory(transactions, filterChoice)}`;
+
   console.log(tempProductList);
 }
 
@@ -248,13 +270,13 @@ function ClearFilters(){
 // 4. BANNER LOGIC
  
 function calculateAvailableInventory(filteredItems) {
-  let totalInventory = 0;
+  let inventoryAvailable = 0;
  
   filteredItems.forEach(item => {
-    totalInventory += item.qtyInMachine;
+    inventoryAvailable += item.qtyInMachine;
   });
  
-  return totalInventory;
+  return inventoryAvailable;
 }
  
  
