a73c733b8fc6d543fc38eb6615506430ddfe642b
uyentruong2003 <uyenkaty.truong@gmail.com>
2023-11-30 21:45:31 -0600

    Uyen hid the connection string

--- API/ConnectionString.cs
+++ API/ConnectionString.cs
@@ -10,11 +10,6 @@ namespace mis321_ttv_web_app
             string username = Environment.GetEnvironmentVariable("USERNAME");
             string password = Environment.GetEnvironmentVariable("PASSWORD");
             string port = Environment.GetEnvironmentVariable("PORT");
-            // string server = "wb39lt71kvkgdmw0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-            // string database = "j426z9qzg7kj50yb";
-            // string username = "jk8kpirk17rvp0o9";
-            // string password = "z473g0sd6byzo8rt";
-            // string port = "3306";
             
             cs = $"server={server};user={username};database={database};port={port};password={password}";
         }
